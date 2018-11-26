bbbea1f2bb8593faeaf40d6ccc6fddcdcd43907d
Eric Callari <ericcallari1@gmail.com>
2018-11-25 23:15:04 -0800

    render and serve module

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 1000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+app.get('/:restaurant_id', (req, res) => {
+  const options = {
+    root: path.join(__dirname, 'public'),
+  };
+  res.sendFile('index.html', options);
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
